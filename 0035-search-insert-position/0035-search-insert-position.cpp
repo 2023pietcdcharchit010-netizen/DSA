3eb3da6c87eb8bbe33e9a52763a9db88372d8a71
charchit sharma <2023pietcdcharchit010@poornima.org>
2026-07-09 11:21:03 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.08%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int n=nums.size();
+       int i=0,j=n-1;
+       while(i<=j){
+        int mid=i+(j-i)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(target<nums[mid]){
+            j=mid-1;
+        }
+        else{
+            i=mid+1;
+        }
+       }
+       return i; 
+    }
+};
