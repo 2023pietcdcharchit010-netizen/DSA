fec2a408727e6bab28088443bd9008a1f491fd6e
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-08-08 09:38:31 +0530

    Time: 186 ms (5.1%), Space: 11.8 MB (15.89%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool fun(vector<int>& have,vector<int> & need){
+        for(int i=0;i<256;i++){
+            if(have[i]<need[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    string minWindow(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        if(n<m){
+            return "";
+        }
+        vector<int> have (256,0);
+        vector<int> need (256,0); 
+        // store frequency of characters in t
+        for(int i=0; i < m;i++){
+            need[t[i]]++;
+        }
+        int low=0;
+        int res=INT_MAX;
+        int start=-1;
+
+        for(int high=0;high<s.length();high++){
+            have[s[high]]++;
+
+            while(fun(have,need)){
+                int len=high-low+1;
+
+            // store minimun window
+                if(len<res){
+                    res=len;
+                    start=low;
+                }
+                //remove left character
+                have[s[low]]--;
+                low++;
+            }
+        }
+        if(start==-1){
+            return "";
+        }
+
+        return s.substr(start,res);
+    }
+};
