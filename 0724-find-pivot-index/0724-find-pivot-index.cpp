9ad61558e748118ae41fae9cace548195d8c1eed
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-08-14 09:18:26 +0530

    Time: 0 ms (100%), Space: 35.7 MB (55.83%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        // calculate total sum
+        for(int num : nums){
+            sum+=num;
+        }
+        int left=0;
+
+        for(int i=0;i<nums.size();i++){
+            int right=sum-nums[i]-left;
+
+            if(left==right){
+                return i;
+            }
+            left+=nums[i];
+        }
+        return -1;
+    }
+};
