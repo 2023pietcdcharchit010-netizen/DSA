856100e94435fe75bd15b8b8d5b30bb851c9f217
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-08-14 10:52:17 +0530

    Time: 59 ms (34.24%), Space: 51.2 MB (8.68%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,13 +6,16 @@ public:
         mp[0]=1;
         int sum=0;
         int count=0;
-        for(int x : nums){
-            sum+=x;
-            
-            if(mp.find(sum-k)!=mp.end()){
-                count+=mp[sum-k];
-            }
-
+        for(int i=0;i<nums.size();i++){
+            //Current PS
+            sum+=nums[i];
+            //Required previous PS
+            int ques=(sum-k);
+            //Frequency of PS
+            int freq=mp[ques];
+            //Add number of valid subarrays
+            count+=freq;
+            //store current PS
             mp[sum]++;
         }
         return count;
