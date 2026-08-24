e7cebb2f3115fd3c64092efd2347338a64adf0b3
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-08-24 09:07:46 +0530

    Time: 3 ms (82.36%), Space: 14.3 MB (49.43%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         stack<char> st;
+        string result = "";
 
         for(int i = 0; i < s.length(); i++) {
 
-            if(!st.empty() && st.top() == s[i]) {
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.top()==s[i]){
                 st.pop();
+                continue;
             }
             else {
                 st.push(s[i]);
             }
         }
-
-        string result = "";
 
         while(!st.empty()) {
             result.push_back(st.top());
