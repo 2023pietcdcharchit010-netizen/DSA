d5554f56c6d12a5c93e574e60e35fa32e0a1b221
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-09-07 15:36:05 +0530

    Time: 0 ms (100%), Space: 23.6 MB (98.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int index=0;
+        int j=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                swap(nums[index],nums[i]);
-                index++;
+                swap(nums[j],nums[i]);
+                j++;
             }
         }
     }
