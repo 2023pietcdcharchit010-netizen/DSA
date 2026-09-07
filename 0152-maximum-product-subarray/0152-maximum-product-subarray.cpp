3c99cc511c159a349cc6aa3e90f45e18a75e646e
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-09-07 18:01:36 +0530

    Time: 0 ms (100%), Space: 17.7 MB (60.24%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,18 +3,17 @@ public:
     int maxProduct(vector<int>& nums) {
         int maxend=nums[0];
         int minend=nums[0];
-        int result=nums[0];
-
+        int maxprodsum=nums[0];
         for(int i=1;i<nums.size();i++){
+            // make possible outcome
             int v1=nums[i];
-            int v2=maxend * nums[i];
-            int v3=minend * nums[i];
+            int v2=nums[i]*maxend;
+            int v3=nums[i]*minend;
 
             maxend=max(v1,max(v2,v3));
             minend=min(v1,min(v2,v3));
-
-            result=max(result,max(maxend,minend));
+            maxprodsum=max(maxprodsum,max(maxend,minend));
         }
-        return result;
+        return maxprodsum;
     }
 };
